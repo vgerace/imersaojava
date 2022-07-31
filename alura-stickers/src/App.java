119d664362659d0c95e1543c65598cd117750887
vgerace <vitorgerace@gmail.com>
2022-07-31 18:02:12 -0300

    Add git ignore e desafios basicos aula 1

--- alura-stickers/src/App.java
+++ alura-stickers/src/App.java
@@ -3,7 +3,6 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
-import java.security.Key;
 import java.util.List;
 import java.util.Map;
 
@@ -12,8 +11,11 @@ public class App {
   public static void main(String[] args) throws Exception {
     // Fazer uma conexão HTTP e buscar os top 250 filmes
 
-    String url = "https://imdb-api.com/API/Top250Movies/k_vqv5snhq";
-    //  String url = "https://imdb-api.com/API/MostPopularMovies/k_vqv5snhq";
+    // String url = "https://imdb-api.com/API/Top250Movies/";
+    String url =
+      "https://imdb-api.com/API/MostPopularMovies/" +
+      System.getProperty("apiKey");
+
     URI endereco = URI.create(url);
     var client = HttpClient.newHttpClient();
     var request = HttpRequest.newBuilder(endereco).GET().build();
@@ -23,20 +25,28 @@ public class App {
     );
     String body = response.body();
     System.out.println(body);
-
     // Extrair os dados que interessam (título, poster, classificação)
-
     var parser = new JsonParser();
-
     List<Map<String, String>> listaDeFilmes = parser.parse(body);
     System.out.println(listaDeFilmes.size());
     System.out.println(listaDeFilmes.get(0));
     //  Exibir e manipular os dados
-
     for (Map<String, String> filme : listaDeFilmes) {
-      System.out.println(filme.get("title"));
-      System.out.println(filme.get("image"));
-      System.out.println(filme.get("imDbRating"));
+      System.out.println(
+        "\u001b[43m\u001b[1m📽️  Título: \u001b[m \u001b[1m \u001b[m" +
+        "\u001b[1m" +
+        filme.get("title")
+      );
+      System.out.println(
+        "\u001b[44m\u001b[1m🖼️  Imagem: \u001b[m \u001b[1m \u001b[m" +
+        "\u001b[1m" +
+        filme.get("image")
+      );
+      System.out.println(
+        "\u001b[41m\u001b[1m🌟 Classificação: \u001b[m \u001b[1m \u001b[m" +
+        "\u001b[1m" +
+        filme.get("imDbRating")
+      );
       System.out.println();
     }
   }
